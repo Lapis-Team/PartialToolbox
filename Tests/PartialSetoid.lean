afe7d39bc75cc7ba33258e16fb92893bf7972c07
Nicolo Pizzo <nicolopizzo@outlook.com>
2026-03-12 11:19:08 +0100

    added example using only `IsProper`

--- Tests/PartialSetoid.lean
+++ Tests/PartialSetoid.lean
@@ -8,43 +8,40 @@ instance natZero : PartialSetoid Nat where
     trans := by grind
   }
 
-instance natZeroRefl {x: Nat} (h : x ≠ 0) : IsProper PartialSetoid.r x where
+instance natZeroRefl {x: Nat} (h : x ≠ 0) : IsProper natZero.r x  where
   _refl := by constructor <;> grind
 
 -- Mettere arrowRelation al posto di arrowPS in modo da non dover usare poi (...).r
 instance natZeroAddIsMorph : IsMorphism natZero (arrowPS natZero natZero) HAdd.hAdd where
   _respects := by
     intro x1 y1 ⟨x1ne, eqx1y1⟩ x2 y2 ⟨x2ne, eqx2y2⟩
-    constructor
-    · grind
-    · grind
-
--- instance natZeroAddIsMorph2 : IsMorphism2 natZero natZero natZero HAdd.hAdd where
---   _respects := by
---     intro x1 y1 x2 y2 ⟨x1ne, eqx1y1⟩ ⟨x2ne, eqx2y2⟩
---     constructor
---     · grind
---     · grind
-
--- instance mulIsMorphism2NatZero : IsMorphism2 natZero natZero natZero HMul.hMul where
---   _respects := by
---     intro x1 y1 x2 y2 ⟨x1ne, eqx1y1⟩ ⟨x2ne, eqx2y2⟩
---     constructor
---     · sorry
---     · grind
+    constructor <;> grind
 
 instance mulIsMorphismNatZero : IsMorphism natZero (arrowPS natZero natZero) HMul.hMul where
   _respects := by
     intro x1 x2 ⟨x1ne, eqx1y1⟩ y1 y2 ⟨x2ne, eqx2y2⟩
     constructor
-    · sorry
+    · exact Nat.mul_ne_zero x1ne x2ne
     · grind
 
 
-variable {p : Nat -> Prop} [IsMorphism natZero psprop p]
+variable {p : Nat -> Prop} [IsMorphism natZero psprop p] [IsProper (arrowPS natZero psprop).r p]
 variable {g : Nat -> Nat -> Nat} [IsMorphism natZero (arrowPS natZero natZero) g]
 variable {g1 : Nat -> Nat -> Nat -> Nat} [IsMorphism natZero (arrowPS natZero (arrowPS natZero natZero)) g1]
 variable {g2 : Nat -> Nat -> Nat -> Nat} [IsProper (arrowPS natZero (arrowPS natZero (arrowPS natZero natZero))).r g2]
+-- variable {q : Nat -> Prop} [IsProper (arrowPS natZero psprop).r q]
+
+def arrow (psa: PartialSetoid α) (psb: PartialSetoid β) (f : α -> β) (g : α -> β) : Prop :=
+  ∀ x y : α, x ≈ y -> f x ≈ g y
+
+def ArrowRelation {α} := Relation (Relation α)
+
+-- def arrowRelation : (ps1 : PartialSetoid α) -> (ps2: PartialSetoid β) -> PartialSetoid (α -> β) :=
+def arrowRelation (f : α -> β) (g : α -> β) [PartialSetoid α] [PartialSetoid β] : Prop :=
+  ∀ x y : α, x ≈ y -> f x ≈ g y
+
+-- variable {g: Nat -> Nat} [IsProper (arrowRelation) g]
+
 
 -- PER over simple predicates
 example : ∀ x y : Nat, natZero.r x y -> p x -> p y := by
@@ -105,7 +102,7 @@ example : ∀ x y z: Nat, z ≠ 0 -> x ≈ y -> p (g1 z x x) -> p (g1 z y y) := 
 example : ∀ x y z: Nat, z ≠ 0 -> natZero.r x y -> p (g2 z x x) -> p (g2 z y y) := by
   intro x y z hz h hx
   apply PartialSetoid.mp
-    (IsMorphism.respects _
+    (IsProper.refl2 (arrowPS _ _) _
         ((IsProper.refl2 (arrowPS _ ( arrowPS _ (arrowPS _ _))) _ (IsProper.refl _ _)) h h))
   apply hx
   apply natZeroRefl hz
