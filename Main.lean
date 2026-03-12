fb8c944e5d5165ee22ac8b4b17ac21831748b386
Nicolo Pizzo <nicolopizzo@outlook.com>
2026-03-12 11:19:22 +0100

    reverted main program

--- Main.lean
+++ Main.lean
@@ -1,11 +1,4 @@
 import PartialSetoid
 import Init.Data.Vector
 
-inductive Vec (α : Type) : Nat -> Type u where
-  | zero : Vec α 0
-  | cons : α -> (Vec α n) -> Vec α (n.succ)
-
-def append (xs : Vec α n) (ys : Vec α m) : Vec α (m + n) :=
-  match xs with
-    | Vec.zero => ys
-    | Vec.cons x xs => Vec.cons x (append xs ys)
+def main : IO Unit := println! "Hello world!"
