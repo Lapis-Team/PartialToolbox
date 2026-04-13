48ddc913985cfb6b2acfca08233b3a7cfe364719
Nicolo Pizzo <nicolopizzo@outlook.com>
2026-04-13 17:45:24 +0200

    minor change

--- PartialToolbox.lean
+++ PartialToolbox.lean
@@ -1,3 +1,7 @@
 -- This module serves as the root of the `PartialSetoid` library.
 -- Import modules here that should be built as part of the library.
--- import PartialSetoid.Basic
+import PartialToolbox.PartialOption
+import PartialToolbox.Partial
+import PartialToolbox.Grw
+import PartialToolbox.ForwardBackward
+
